46ddebe121052f697e17ea1440351bbfc613ee2a
Alex Ivanov <ivanov.alex1@gmail.com>
2020-07-11 16:05:12 +0300

    Creator done

---
+++ script.js
@@ -0,0 +1,15 @@
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+
+let qrcode = new QRCode(document.querySelector('#qrcode'), {
+    width: 250,
+    height: 250,
+    colorDark : "#000000",
+    colorLight : "#ffffff",
+    correctLevel : QRCode.CorrectLevel.H
+});
+
+button.addEventListener('click', () => {
+    let inputValue = input.value;
+    qrcode.makeCode(inputValue)
+})
